a58c8d5cde8967895808efec5c6b9723c27d4a20
hsabrina1 <162997288+hsabrina1@users.noreply.github.com>
2024-08-23 09:51:10 +0200

    Add files via upload

---
+++ portfolio/_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="./Assets/Css/header.css" rel="stylesheet"
+</head>
+<body>
+<p>Accueil 
+Profil
+Compétences
+Portfolio
+Contact </p>
+</body>
+</html>
